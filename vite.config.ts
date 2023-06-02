541c680a89401fcafe6f3b25629c8e0cc72f40e9
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2023-06-02 20:44:41 +0900

    try to fix "unexpected usage" error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import monacoEditorPlugin from "vite-plugin-monaco-editor";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,9 @@ export default defineConfig({
     monacoEditorPlugin.default({
       languageWorkers: ["editorWorkerService"],
       customWorkers: [{ label: "yaml", entry: "monaco-yaml/yaml.worker.js" }],
+      customDistPath(root, buildOutDir) {
+        return path.join(root, buildOutDir, "monacoeditorwork");
+      },
     }),
   ],
 });
